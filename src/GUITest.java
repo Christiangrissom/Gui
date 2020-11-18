b4cd1e005a16a510e120845800e088c49a28ca03
Christian <christiangrissom009@gmail.com>
2020-11-18 16:51:34 -0600

    It looks great

--- src/GUITest.java
+++ src/GUITest.java
@@ -7,9 +7,40 @@ import javax.swing.JFrame;
 
 public class GUITest {
 	
+	private static int counter = 0;
 	
-	
-	
+	private static ActionListener b1A = new  ActionListener(){
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Don't do that.");
+		}
+	};
+	private static ActionListener b2A = new  ActionListener(){
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Leave the goats alone.");
+		}
+	};
+	private static ActionListener b3A = new  ActionListener(){
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("mokey.");
+		}
+	};
+	private static ActionListener b4A = new  ActionListener(){
+		public void actionPerformed(ActionEvent e) {
+			if(counter>99) {
+				System.out.println("Done");
+			}
+			else {
+				counter++;
+				JFrame badFrame = new JFrame("Good luck");
+				badFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				badFrame.setSize(100,100);
+				badFrame.setLocationRelativeTo(null);
+				badFrame.setVisible(true);
+
+				actionPerformed(e);
+			}
+		}
+	};
 	private static final int HEIGHT = 150;
 	private static final int WIDTH = 400;
 
@@ -25,12 +56,10 @@ public class GUITest {
 		
 		
 		
-		b1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Don't do that.");
-			}
-			
-		});
+		b1.addActionListener(b1A);
+		b2.addActionListener(b2A);
+		b3.addActionListener(b3A);
+		b4.addActionListener(b4A);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		FlowLayout layout = new FlowLayout();
